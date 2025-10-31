44103116e8ab1617938f4eefc2503513bc51845b
Tejeshwini R S <70053570+TejeshwiniRS@users.noreply.github.com>
2025-10-31 01:45:44 -0500

    Time: 3058 ms (6.49%), Space: 17.9 MB (39.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,11 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        mp={}
         if(len(s1)>len(s2)):
             return False
-        s1 = ''.join(sorted(s1))
+        for s in s1:
+            mp[s] = mp.get(s, 0)+1
         l,r=0,len(s1)-1
         while(r<len(s2)):
-            if(s1 == ''.join(sorted(s2[l:r+1]))):
+            mp1={}
+            k=l
+            while(k<=r):
+                mp1[s2[k]] = mp1.get(s2[k], 0)+1
+                k+=1
+            if(mp==mp1):
                 return True
             l+=1
             r+=1
